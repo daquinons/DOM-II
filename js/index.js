8ca9d76c4377e221424352723c25407554939800
David Quiñones <daquinons@users.noreply.github.com>
2019-05-14 15:04:31 +0200

    Add the rest of the events

--- js/index.js
+++ js/index.js
@@ -36,7 +36,7 @@ document.addEventListener('keydown', () => {
 
 // 4. Wheel event
 
-let mapImage = document.querySelector('.img-content img');
+let mapImage = document.querySelectorAll('.img-content img')[0];
 let rotate = 0;
 document.addEventListener('wheel', () => {
   rotate += 45;
@@ -44,4 +44,66 @@ document.addEventListener('wheel', () => {
     rotate = 0;
   }
   mapImage.setAttribute('style',`transform:rotate(${rotate}deg)`);
+})
+
+// 5. Dragstart event
+
+let veniceImage = document.querySelectorAll('.img-content img')[1];
+veniceImage.addEventListener('dragstart', () => {
+  veniceImage.style.visibility = 'hidden';
+})
+
+// 6. Doubleclick event
+
+let veniceImageContainer = document.querySelectorAll('.img-content')[1];
+veniceImageContainer.addEventListener('dblclick', () => {
+  veniceImage.style.visibility = 'visible';
+})
+
+// 7. Click event
+
+let thaiBoatImage = document.querySelector('.content-destination img');
+let rotateSecond = 0;
+thaiBoatImage.addEventListener('click', () => {
+  rotateSecond += 22.5;
+  if (rotateSecond > 360) {
+    rotateSecond = 0;
+  }
+
+  thaiBoatImage.setAttribute('style',`transform:rotate(${rotateSecond}deg)`);
+})
+
+// 8. Mouseup event
+
+function getSelectionText() {
+  let text = "";
+  let selection = window.getSelection();
+  if (selection) {
+      text = window.getSelection().toString();
+      console.log(window.getSelection());
+  } else if (document.selection && document.selection.type != "Control") {
+      text = document.selection.createRange().text;
+  }
+  return text;
+}
+
+document.body.addEventListener('mouseup', () => {
+  let selectedText = getSelectionText();
+  if (selectedText.length > 0) {
+    alert('Selected text: ' + selectedText);
+  }
+})
+
+// 9. Load event
+
+window.addEventListener('load', () => {
+  let logo = document.querySelector('.logo-heading');
+  logo.style.color = 'red';
+})
+
+// 10. Keyup event
+
+document.addEventListener('keyup', () => {
+  let buttons = document.querySelectorAll('.btn');
+  buttons.forEach((button) => button.style.backgroundColor = "#"+((1<<24)*Math.random()|0).toString(16));
 })
